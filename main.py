97c82716fb6922e4bf243d86f335eb8dca53eef4
dominika05 <u64763@student.wat.edu.pl>
2025-06-23 11:55:54 +0200

    wip: map user functionality

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 if __name__ == '__main__':
     main()
 from utils.model import users
-from utils.controller import get_user_info, add_user, remove_user, update_user
+from utils.controller import get_user_info, add_user, remove_user, update_user, get_map
 
 
 def main():
@@ -19,6 +19,7 @@ def main():
     print('2 - dodaj znajomego')
     print('3 - usuń znajomego')
     print('4 - zauktualizuj dane znajomego')
+    print('5 - Wygeneruj mapę znajomych')
     print('=============================')
     while True:
         choice: str = input('wybierz opcję MENU: ')
@@ -27,6 +28,7 @@ def main():
         if choice == '2': add_user(users)
         if choice == '3': remove_user(users)
         if choice == '4': update_user(users)
+        if choice == '5': get_map(users)
 
 
 if __name__ == '__main__':
