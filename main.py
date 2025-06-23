b4a61b6955ce934b3ba22d3efd16cd01007265c4
dominika05 <u64763@student.wat.edu.pl>
2025-06-23 11:43:31 +0200

    wip: remove new user functionality

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
 if __name__ == '__main__':
     main()
 from utils.model import users
-from utils.controller import get_user_info, add_user
+from utils.controller import get_user_info, add_user, remove_user
 
 
 def main():
@@ -17,12 +17,14 @@ def main():
     print('0 - zakończ program')
     print('1 - wyświetl co u znajomych')
     print('2 - dodaj znajomego')
+    print('3 - usuń znajomego')
     print('=============================')
     while True:
         choice: str = input('wybierz opcję MENU: ')
         if choice == '0': break
         if choice == '1': get_user_info(users)
         if choice == '2': add_user(users)
+        if choice == '3': remove_user(users)
 
 
 if __name__ == '__main__':
